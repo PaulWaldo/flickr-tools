f106a8cd2d0659e87e5167f14ec6eb96c4e6519c
Paul Waldo <paul.w.waldo@gmail.com>
2021-09-29 16:29:10 -0400

    Puts general functions in utils

--- cmd/random-fav/random-fav.go
+++ cmd/random-fav/random-fav.go
@@ -3,66 +3,24 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"math/rand"
-	"net/http"
-	"net/url"
-	"os"
 	"strconv"
-	"strings"
 	"time"
 
+	"github.com/PaulWaldo/flickr-tools/utils"
 	flickr "github.com/azer/go-flickr"
 )
 
-func downloadFile(fullURLFile string) (string, error) {
-	const noFile = ""
-	fileURL, err := url.Parse(fullURLFile)
-	if err != nil {
-		return noFile, err
-	}
-	path := fileURL.Path
-	segments := strings.Split(path, "/")
-	fileName := segments[len(segments)-1]
-
-	// Create blank file
-	file, err := os.Create(fileName)
-	if err != nil {
-		return noFile, err
-	}
-
-	client := http.Client{}
-	resp, err := client.Get(fullURLFile)
-	if err != nil {
-		return noFile, err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		return noFile, fmt.Errorf("unable to download %s : got response %s", fullURLFile, resp.Status)
-	}
-
-	size, err := io.Copy(file, resp.Body)
-	if err != nil {
-		return noFile, err
-	}
-	defer file.Close()
-
-	sLog(fmt.Sprintf("Downloaded file %s with size %d bytes", fileName, size))
-	return fileName, nil
-}
-
 var (
-	downloadDir string
-	userName    string
-	apiKey      string
-	envFile     string
-	verbose     bool
-	size        int
+	userName string
+	apiKey   string
+	envFile  string
+	size     int
 )
 
 func setupFlags() {
-	flag.StringVar(&downloadDir, "dir", ".", "Directory of the downloaded image")
+	flag.StringVar(&utils.DownloadDir, "dir", ".", "Directory of the downloaded image")
 	flag.StringVar(&userName, "user", "", "Name of user for which to load favorites")
 	flag.StringVar(&apiKey, "key", "", "Flickr API Key (see https://www.flickr.com/services/apps/create/).  "+
 		"May be specified in an evironment file (see 'env' option)")
@@ -70,17 +28,10 @@ func setupFlags() {
 		"Name of an environment file that contains the %s value.  "+
 			"If omitted and no \"key\" argument specified, uses \"./.env\" for enviroment file.",
 		flickr.ApiKeyEnvVar))
-	flag.BoolVar(&verbose, "v", false, "Verbose")
+	flag.BoolVar(&utils.Verbose, "v", false, "Verbose")
 	flag.IntVar(&size, "size", 2048, "Desired size of the long edge of the image.  "+
 		"Resultant image may be larger if size does not exist.")
 	flag.Parse()
-}
-
-// sLog is a simple logger based on the verbose flag
-func sLog(msg string) {
-	if verbose {
-		log.Println(msg)
-	}
 }
 
 func main() {
@@ -93,14 +44,14 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error finding user %s: %s", userName, err)
 	} else {
-		sLog(fmt.Sprintf("ID is %s", user.Id))
+		utils.SLog(fmt.Sprintf("ID is %s", user.Id))
 	}
 
 	fav, err := client.RandomFav(user.Id)
 	if err != nil {
 		log.Fatalf("Unable to get random fav: %s", err)
 	}
-	sLog(fmt.Sprintf("Got \"%s\"", fav.Title))
+	utils.SLog(fmt.Sprintf("Got \"%s\"", fav.Title))
 
 	favId, err := strconv.Atoi(fav.Id)
 	if err != nil {
@@ -117,7 +68,7 @@ func main() {
 	}
 
 	var file string
-	file, err = downloadFile(url)
+	file, err = utils.DownloadFile(url)
 	if err != nil {
 		log.Fatalf("Unable to download URL %s : %s", url, err)
 	}
---
+++ utils/utils.go
@@ -0,0 +1,57 @@
+package utils
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+var DownloadDir string
+var Verbose bool
+
+// SLog is a simple logger based on the verbose flag
+func SLog(msg string) {
+	if Verbose {
+		log.Println(msg)
+	}
+}
+
+func DownloadFile(fullURLFile string) (string, error) {
+	const noFile = ""
+	fileURL, err := url.Parse(fullURLFile)
+	if err != nil {
+		return noFile, err
+	}
+	path := fileURL.Path
+	segments := strings.Split(path, "/")
+	fileName := segments[len(segments)-1]
+
+	// Create blank file
+	file, err := os.Create(fileName)
+	if err != nil {
+		return noFile, err
+	}
+
+	client := http.Client{}
+	resp, err := client.Get(fullURLFile)
+	if err != nil {
+		return noFile, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return noFile, fmt.Errorf("unable to download %s : got response %s", fullURLFile, resp.Status)
+	}
+
+	size, err := io.Copy(file, resp.Body)
+	if err != nil {
+		return noFile, err
+	}
+	defer file.Close()
+
+	SLog(fmt.Sprintf("Downloaded file %s with size %d bytes", fileName, size))
+	return fileName, nil
+}
